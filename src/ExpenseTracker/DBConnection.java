cacf76f407eb5f6ad3586472bffdeceab864128a
Vinay Kumar Soni <vinaykumarsoni449@gmail.com>
2025-09-27 10:54:51 +0530

    Update DBConnection.java

--- src/ExpenseTracker/DBConnection.java
+++ src/ExpenseTracker/DBConnection.java
@@ -4,8 +4,8 @@ import java.sql.*;
 
 public class DBConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/expense_tracker";
-    private static final String USER = "root"; 
-    private static final String PASSWORD = "vinay_kumar@&1976"; 
+       String user = System.getenv("DB_USER");   
+       String pass = System.getenv("DB_PASS");  
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
